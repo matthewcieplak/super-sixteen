3eced21a3843f0f8811cbc8cc08dc00eed0806d3
Matthew Cieplak <matthew.cieplak@gmail.com>
2019-06-03 22:27:11 -0700

    Debugging analog i/o, making pots reach max easier

--- sequencer_app_v2/src/AnalogIO.cpp
+++ sequencer_app_v2/src/AnalogIO.cpp
@@ -8,6 +8,13 @@
 #include "Display.h"
 #include "AnalogIO.h"
 
+int analog_pins[4] = { 
+	ANALOG_PIN_1,
+	ANALOG_PIN_2,
+	ANALOG_PIN_3,
+	ANALOG_PIN_4
+};
+
 int analog_params[4] = { PITCH_PARAM, OCTAVE_PARAM, DURATION_PARAM, CV_PARAM };
 
 int display_param = PITCH_PARAM;
@@ -44,21 +51,21 @@ void setOutput(uint8_t channel, uint8_t gain, uint8_t shutdown, unsigned int val
 	uint8_t highByte = ((val >> 8) & 0xff) | channel << 7 | gain << 5 | shutdown << 4;
 
 	//PORTB &= 0xfb;
-	digitalWrite(CS2_PIN, LOW);
+	digitalWrite(CS3_PIN, LOW);
 	SPI.transfer(highByte);
 	SPI.transfer(lowByte);
-	digitalWrite(CS2_PIN, HIGH);
+	digitalWrite(CS3_PIN, HIGH);
 }
 
 
 void read_input() {
 	analogMultiplexor += 1;
-	if (analogMultiplexor > 4) {
+	if (analogMultiplexor > 3) {
 		analogMultiplexor = 0;
 	}
 	int i = analogMultiplexor;
-	if (i > 3 || i < 0) return; //sometimes we get desynced by interrupts, and analogRead on a wrong pin is fatal
-	analogValues[i] = analogRead(i);
+	// if (i > 3 || i < 0) return; //sometimes we get desynced by interrupts, and analogRead on a wrong pin is fatal
+	analogValues[i] = analogRead(analog_pins[i]);
 
 	int change_threshold = 100;
 	if (display_param == analog_params[i]) {
@@ -72,35 +79,34 @@ void read_input() {
 		case 2: setDuration(analogValues[2]); break;
 		case 3: setCV(analogValues[3]); break;
 		}
-		setDisplayNum();
 	}
 }
 
 void setPitch(int analogValue) {
 	display_param = PITCH_PARAM;
 	//calibration_value = (float(analogValues[1]) + 1024.0) / 1500.0 ; //11.60
-	int newVal = analogValue / 42.6 - 12; //convert from 0_1024 to 0_88 to -12_0_12
-	if (pitch_matrix[selected_step] != newVal) num_display = newVal;
+	int newVal = analogValue / 42.1 - 12.1; //convert from 0_1024 to 0_88 to -12_0_12
+	if (pitch_matrix[selected_step] != newVal) setDisplayNum(newVal);
 	pitch_matrix[selected_step] = newVal;
 }
 
 void setOctave(int analogValue) {
 	display_param = OCTAVE_PARAM;
 	int newVal = analogValue / 120 - 4; //convert from 0-1024 to -4_0_4
-	if (octave_matrix[selected_step] != newVal) num_display = newVal;
+	if (octave_matrix[selected_step] != newVal) setDisplayNum(newVal);
 	octave_matrix[selected_step] = newVal;
 }
 
 void setDuration(long analogValue) { //need extra bits for exponent operation
 	display_param = DURATION_PARAM;
 	int newVal = analogValue * analogValue / 2615; //convert from 0-1024 to 0-400 with exponential curve
-	if (duration_matrix[selected_step] != newVal) num_display = newVal;
+	if (duration_matrix[selected_step] != newVal) setDisplayNum(newVal);
 	duration_matrix[selected_step] = newVal;
 }
 void setCV(int analogValue) {
 	display_param = CV_PARAM;
 	int newVal = analogValue / 10.23; //convert from 0-1024 to 0-100
-	if (cv_matrix[selected_step] != newVal) num_display = newVal;
+	if (cv_matrix[selected_step] != newVal) setDisplayNum(newVal);
 	cv_matrix[selected_step] = newVal;
 }
 
@@ -108,10 +114,10 @@ void displaySelectedParam() {
 	//update display to show currently selected step value if applicable
 	switch (display_param) {
 		//case TEMPO_PARAM: break
-	case PITCH_PARAM:    num_display = pitch_matrix[selected_step]; break;
-	case OCTAVE_PARAM:   num_display = octave_matrix[selected_step]; break;
-	case DURATION_PARAM: num_display = duration_matrix[selected_step]; break;
-	case CV_PARAM:       num_display = cv_matrix[selected_step]; break;
+	case PITCH_PARAM:    setDisplayNum(pitch_matrix[selected_step]); break;
+	case OCTAVE_PARAM:   setDisplayNum(octave_matrix[selected_step]); break;
+	case DURATION_PARAM: setDisplayNum(duration_matrix[selected_step]); break;
+	case CV_PARAM:       setDisplayNum(cv_matrix[selected_step]); break;
 	//case CALIBRATION_PARAM: num_display = calibration_values[selected_step]; break;
 	}
 }
