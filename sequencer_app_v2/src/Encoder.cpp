385b1b57d593c31abc530f0190978901c691db7b
Matthew Cieplak <matthew.cieplak@gmail.com>
2020-02-28 18:45:16 -0800

    Adding encoder invert function

--- sequencer_app_v2/src/Encoder.cpp
+++ sequencer_app_v2/src/Encoder.cpp
@@ -8,14 +8,23 @@
 #include "Arduino.h"
 #include <stdint.h>
 
+#include <EEPROM.h>
+
+
 namespace supersixteen{
 
 int increment_amount = 0;
 int encoder_amount = 0;
 
+uint8_t pinA = 0xFF;
+uint8_t pinB = 0xFF;
+int8_t invert_encoder = 1;
+const int encoder_invert_address = 16;
+
 void Encoder::init(){
 	pinMode(ENC_A_PIN, INPUT_PULLUP); //encoder A
 	pinMode(ENC_B_PIN, INPUT_PULLUP); //encoder B
+	invert_encoder = EEPROM.read(encoder_invert_address);
 }
 
 //magic numbers from https://www.circuitsathome.com/mcu/reading-rotary-encoder-on-arduino/
@@ -29,9 +38,11 @@ int Encoder::poll(){
 	/**/
 	old_AB <<= 2;                   //remember previous state
 	old_AB |= (ENC_PORT & 0x30) >> 4;  //add current state from pins A5 and A4, shifted to LSB
-	encoder_increment(enc_states[(old_AB & 0x0f)] * -1); //extract encrement/decrement value from state table
-	return increment_amount;
+	encoder_increment(enc_states[(old_AB & 0x0f)]); //extract encrement/decrement value from state table
+	return increment_amount * invert_encoder;
 }
+
+
 
 void Encoder::encoder_increment(int amt) {
 	if (amt == 0) return;
@@ -39,6 +50,11 @@ void Encoder::encoder_increment(int amt) {
 	if (abs(encoder_amount) < 4) return; //encoder detent is too coarse for 1-per step granularity
 	increment_amount = encoder_amount > 0 ? 1 : -1;
 	encoder_amount = 0;
+}
+
+void Encoder::toggle_inverted(){
+	invert_encoder = invert_encoder * -1;
+	EEPROM.write(encoder_invert_address, invert_encoder);
 }
 
 }
--- sequencer_app_v2/src/Encoder.h
+++ sequencer_app_v2/src/Encoder.h
@@ -8,8 +8,10 @@ class Encoder{
     public:
         void init();
         int poll();
+        int smartPoll();
         int encoder_amount = 0;
         int increment_amount = 0;
+        void toggle_inverted();
     private:
         void encoder_increment(int amt);
 };
