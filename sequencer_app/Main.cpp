43f28679b30333f807d2bf6749e5e5044e65100b
Matthew Cieplak <matthew.cieplak@gmail.com>
2020-11-10 14:35:20 -0800

    Compiling old sequencer app

--- sequencer_app/Main.cpp
+++ sequencer_app/Main.cpp
@@ -1,43 +1,45 @@
-#include <Arduino.h>
-#include <string.h>
-#include <MCP23S17.h> //https://github.com/MajenkoLibraries/MCP23S17
+//#include <Arduino.h>
+//#include <string.h>
+//#include <MCP23S17.h> //https://github.com/MajenkoLibraries/MCP23S17
 //#include <SPI.h>
 //#include <elapsedMillis.h>
-
-
+//
+//#include "Analogio.h"
 //#include "Buttons.h"
+//#include "Calibrate.h"
 //#include "Display.h"
+//#include "Encoder.h"
 //#include "Font.h"
 //#include "LEDMatrix.h"
 //#include "Sequencer.h"
 //#include "Variables.h"
 //#include "Pinout.h"
-
-void setup() {
-	analogReference(EXTERNAL); // use AREF for reference voltage
-	pinMode(CS0_PIN, OUTPUT); //enable CS for DAC
-	pinMode(CS1_PIN, OUTPUT); //enable CS for DAC
-
-	initializeMatrix();
-	initializeDisplay();
-	initializeFont();
-	initializeButtons();
-
-	for (int i = 0; i < 16; i++) {
-		duration_matrix[i] = 80;
-	}
-}
-
-void loop() {
-	increment_step();
-	multiplex_leds();
-	update_gate();
-
-	if (stepper > 10) { //save cycles and do these less frequently
-		stepper = 0;
-		read_encoder();
-		blink_step();
-		read_input();
-	}
-
-}
+//
+//void setup() {
+//	analogReference(EXTERNAL); // use AREF for reference voltage
+//	pinMode(CS0_PIN, OUTPUT); //enable CS for DAC
+//	pinMode(CS1_PIN, OUTPUT); //enable CS for DAC
+//
+//	initializeMatrix();
+//	initializeDisplay();
+//	initializeFont();
+//	initializeButtons();
+//
+//	for (int i = 0; i < 16; i++) {
+//		duration_matrix[i] = 80;
+//	}
+//}
+//
+//void loop() {
+//	increment_step();
+//	multiplex_leds();
+//	update_gate();
+//
+//	if (stepper > 10) { //save cycles and do these less frequently
+//		stepper = 0;
+//		read_encoder();
+//		blink_step();
+//		read_input();
+//	}
+//
+//}
